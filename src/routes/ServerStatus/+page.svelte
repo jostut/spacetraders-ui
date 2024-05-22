6d20996aba86260fe136f839eb142a2486918f80
jostut <JGStutson@gmail.com>
2024-05-22 00:39:35 -0600

    Updated server status page with more user friendly text

--- src/routes/ServerStatus/+page.svelte
+++ src/routes/ServerStatus/+page.svelte
@@ -1,28 +1,30 @@
-<svelte:head>
-	<title>SpaceTraders Server Status</title>
-</svelte:head>
 <script lang="ts">
+	import { Badge, Heading, P, Span } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
 	import type {GetStatus200Response} from '../../../packages/spacetraders-sdk/models/get-status200-response';
 	let data: GetStatus200Response;
-	let status: string;
+	let status: 'green'|'red' = 'red';
 	onMount(async () => {
 		data = await fetch('https://api.spacetraders.io/v2').then((x) => x.json());
-		status = data.status == "SpaceTraders is currently online and available to play" ? "Green" : "Red";
+		if(data.status == "SpaceTraders is currently online and available to play"){
+			status = "green"
+		};
 	});
 </script>
 
-<h1>SpaceTraders Server Status</h1>
-{#if data !== undefined}
-<p>Server Status: {status}</p>
-<p>Version: {data.version}</p>
+<svelte:head>
+	<title>SpaceTraders Server Status</title>
+</svelte:head>
 
-<h3>Reset Date information</h3>
-<p>Last server reset was: {new Date(data.resetDate).toLocaleDateString()}</p>
-<p>Next reset is on: {new Date(data.serverResets.next).toLocaleDateString()}</p>
-{:else}
-<p>Server Status:</p>
-<p>Version:</p>
-<p>Last server reset was:</p>
-<p>Next reset is on:</p>
-{/if}
+<Heading tag='h1'>SpaceTraders Server Status</Heading>
+<div class="indent-4 mb-4">
+	<P><Span>Server Status:</Span> <Badge rounded color={status} class="capitalize indent-0">{status}</Badge></P>
+	<P>Version: {data?.version}</P>
+</div>
+<div>
+	<Heading tag=h3>Reset Date information</Heading>
+	<div class="indent-4">
+		<P>Last server reset was: {new Date(data?.resetDate).toLocaleDateString()}</P>
+		<P>Next reset is on: {new Date(data?.serverResets.next).toLocaleDateString()}</P>
+	</div>
+</div>
