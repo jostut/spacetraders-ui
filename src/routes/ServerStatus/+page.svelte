27755bff062e10363acd9b662acf189d7e044b9f
jostut <JGStutson@gmail.com>
2024-05-21 22:46:50 -0600

    Added basis server status data to server status page

--- src/routes/ServerStatus/+page.svelte
+++ src/routes/ServerStatus/+page.svelte
@@ -5,14 +5,24 @@
 	import { onMount } from 'svelte';
 	import type {GetStatus200Response} from '../../../packages/spacetraders-sdk/models/get-status200-response';
 	let data: GetStatus200Response;
+	let status: string;
 	onMount(async () => {
 		data = await fetch('https://api.spacetraders.io/v2').then((x) => x.json());
+		status = data.status == "SpaceTraders is currently online and available to play" ? "Green" : "Red";
 	});
 </script>
 
 <h1>SpaceTraders Server Status</h1>
-<p>This will list information about the server</p>
+{#if data !== undefined}
+<p>Server Status: {status}</p>
+<p>Version: {data.version}</p>
 
-<pre>
-  {JSON.stringify(data, null, 2)}
-</pre>
+<h3>Reset Date information</h3>
+<p>Last server reset was: {new Date(data.resetDate).toLocaleDateString()}</p>
+<p>Next reset is on: {new Date(data.serverResets.next).toLocaleDateString()}</p>
+{:else}
+<p>Server Status:</p>
+<p>Version:</p>
+<p>Last server reset was:</p>
+<p>Next reset is on:</p>
+{/if}
